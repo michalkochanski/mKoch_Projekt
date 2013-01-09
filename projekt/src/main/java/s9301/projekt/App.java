543bef900491d093a62d5acdb64792bc64eb7eac
Michał Kochański <info@mkoch.pl>
2013-01-09 10:59:25 +0100

    first commit

---
+++ projekt/src/main/java/s9301/projekt/App.java
@@ -0,0 +1,13 @@
+package s9301.projekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
