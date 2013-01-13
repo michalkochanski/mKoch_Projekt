1fb3066c90545e4734aa57f9974613343d77c2ba
Michał Kochański <info@mkoch.pl>
2013-01-13 02:02:11 +0100

    testin how git works

--- projekt/src/main/java/s9301/projekt/main.java
+++ projekt/src/main/java/s9301/projekt/main.java
@@ -3,6 +3,6 @@ package s9301.projekt;
 public class main {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!!" );
     }
 }
