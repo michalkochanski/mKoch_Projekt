70a06c4b76a152afb509175ce7ed26e311a3297e
Michał Kochański <info@mkoch.pl>
2013-01-14 21:27:37 +0100

    BAZA!!

--- projekt/src/main/java/s9301/projekt/main.java
+++ projekt/src/main/java/s9301/projekt/main.java
@@ -3,6 +3,10 @@ package s9301.projekt;
 public class main {
     public static void main( String[] args )
     {
+    	DBconnector db = new DBconnector();
+    	
+    	db.baza();
+    	
         System.out.println( "Hello World!!!" );
     }
 }
