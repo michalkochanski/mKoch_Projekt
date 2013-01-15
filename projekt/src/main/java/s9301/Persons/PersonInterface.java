590239710115363d191b7fb28a873c1020f14026
Michał Kochański <info@mkoch.pl>
2013-01-15 22:25:29 +0100

    PersonInterface ended plus small improvements. Good Night! :)

--- projekt/src/main/java/s9301/Persons/PersonInterface.java
+++ projekt/src/main/java/s9301/Persons/PersonInterface.java
@@ -1,8 +1,16 @@
 package s9301.Persons;
 
 public interface PersonInterface {
+	public String getIndex();
 	public String getFirstName();
+	public String getLastName();
+	public String getYearOfBorn();
+	public String getAbout();
 
+	public void setIndex(int index);
 	public void setFirstName(String firstName);
+	public void setLastName(String lastName);
+	public void setYearOfBorn(String yearOfBorn);
+	public void setAbout(String about);
 
 }
--- projekt/src/main/java/s9301/Persons/StudentClass.java
+++ projekt/src/main/java/s9301/Persons/StudentClass.java
@@ -7,6 +7,15 @@ public class StudentClass implements PersonInterface {
 	private String lastName;
 	private String yearOfBorn;
 	private String about;
+	
+	public StudentClass()
+	{
+		int index = 0;
+		String firstName = "";
+		String lastName = "";
+		String yearOfBorn = "";
+		String about = "";
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -14,6 +23,46 @@ public class StudentClass implements PersonInterface {
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+
+	public String getIndex() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getYearOfBorn() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAbout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setIndex(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setLastName(String lastName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setYearOfBorn(String yearOfBorn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAbout(String about) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
